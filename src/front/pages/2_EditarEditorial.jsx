f624e3000555f7e841f1b24b1da4f183a22b01df
DanielGoSi97 <112876178+DanielGoSi97@users.noreply.github.com>
2026-03-23 16:01:39 +0000

    CRUD editorial

---
+++ src/front/pages/2_EditarEditorial.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const EditarEditorial = () => {
+
+    const [editoriales, setEditoriales] = useState([])
+
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default EditarEditorial
